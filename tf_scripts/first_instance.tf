4605b8e777fd60b758f27a1bc714a52bf5d82d4c
Bond007 <nishanth.hg716@gmail.com>
2023-06-11 18:57:58 +0530

    ec2 instance script added update

--- tf_scripts/first_instance.tf
+++ tf_scripts/first_instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "intro" {
   ami = "ami-049a62eb90480f276"
   instance_type = "t2.micro"
   availability_zone = "ap-south-1a"
-  key_name = nexus
+  key_name = "nexus"
   vpc_security_group_ids = ["sg-0c64a2400436ce6b9"]
   tags = {
     Name = "dev-instance"
